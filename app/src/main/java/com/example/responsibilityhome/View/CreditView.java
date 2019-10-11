b639c31f996ae49b06bd95f7bec03581d3575b20
kimyoungjin12 <qazyj@naver.com>
2019-10-12 01:43:48 +0900

    commit

--- app/src/main/java/com/example/responsibilityhome/View/CreditView.java
+++ app/src/main/java/com/example/responsibilityhome/View/CreditView.java
@@ -4,8 +4,11 @@ package com.example.responsibilityhome.View;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
@@ -14,16 +17,34 @@ import com.example.responsibilityhome.R;
 import com.example.responsibilityhome.ResponsibilityDetailActivity;
 
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+
 import android.view.View;
 
 import org.eazegraph.lib.charts.StackedBarChart;
 import org.eazegraph.lib.models.BarModel;
 import org.eazegraph.lib.models.StackedBarModel;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class CreditView extends Activity {
     private PieView mPieView;
+
+    String myJSON;
+
+    private static final String TAG_AMOUNT = "result";
+
+    JSONArray posts = null;
+    String userData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +55,16 @@ public class CreditView extends Activity {
         TextView name = (TextView)findViewById(R.id.name);
         name.setText("2등급");
 
+        GetDataJSON g = new GetDataJSON();
+        try {
+            userData = g.execute("http://49.236.135.136/holding_account_select.php").get();
+            Log.d("yeop", ""+userData);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+/*
         //막대그래프 추가
         StackedBarChart mStackedBarChart = findViewById(R.id.stackedbarchart);
 
@@ -74,8 +105,44 @@ public class CreditView extends Activity {
                 startActivity(intent);
             }
         });
-
+*/
     }
+
+       class GetDataJSON extends AsyncTask<String, Void, String> {
+            @Override
+           protected String doInBackground(String... params) {
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    String userData="";
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+                    myJSON = sb.toString().trim();
+                    Log.d("yeop", "what1");
+                    JSONObject jsonObj = new JSONObject(myJSON);
+                    posts = jsonObj.getJSONArray(TAG_AMOUNT);
+                    for (int i = 0; i < posts.length(); i++) {
+                        JSONObject c = posts.getJSONObject(i);
+                        if (c.getString("joinKey").equals("youngjinBBam")) {
+                                userData += ""+ c.getString("data") + "/" + c.getString("resAccountBalance") + "/" + c.getString("resOverdraftAcctYN") + "/" + c.getString("resAccountNickName") + "/";
+                        }
+                    }
+                    return userData;
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+            @Override
+           protected void onPostExecute(String result) {
+           }
+       }
 
     private void initPieView() {
         mPieView.setColors(createColors());
@@ -84,15 +151,15 @@ public class CreditView extends Activity {
 
     private ArrayList<PieEntry> createData() {
         ArrayList<PieEntry> pieLists = new ArrayList<>();
-        pieLists.add(new PieEntry(20.00F, "양념치킨"));
+        pieLists.add(new PieEntry(20.00F, "상환이력"));
         pieLists.add(new PieEntry(20.00F, ""));
-        pieLists.add(new PieEntry(5.00F, "간장치킨"));
+        pieLists.add(new PieEntry(5.00F, "재정수준"));
         pieLists.add(new PieEntry(0.00F, ""));
-        pieLists.add(new PieEntry(10.00F, "후라이드"));
+        pieLists.add(new PieEntry(10.00F, "거래기간"));
         pieLists.add(new PieEntry(5.00F, ""));
-        pieLists.add(new PieEntry(10.00F, "닭강정"));
+        pieLists.add(new PieEntry(10.00F, "대출"));
         pieLists.add(new PieEntry(20.00F, ""));
-        pieLists.add(new PieEntry(5.00F, "뿌링클"));
+        pieLists.add(new PieEntry(5.00F, "임의"));
         pieLists.add(new PieEntry(5.00F, ""));
         return pieLists;
     }
